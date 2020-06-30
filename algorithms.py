a31b3ac0508cff8e6cc3096064b33890c314ff29
Anupam96786 <anupam96786@gmail.com>
2020-07-01 04:17:20 +0530

    range added

--- algorithms.py
+++ algorithms.py
@@ -1,20 +1,20 @@
-def mystd(lst):
+def std(lst):
     mean = sum(lst)/len(lst)
     std = (sum([(i-mean)**2 for i in lst])/len(lst))**0.5
     return std
 
 
-def myvar(lst):
+def var(lst):
     mean = sum(lst)/len(lst)
     var = sum([(i-mean)**2 for i in lst])/len(lst)
     return var
 
 
-def mymean(lst):
+def mean(lst):
     return sum(lst)/len(lst)
 
 
-def mymedian(lst):
+def median(lst):
     lst.sort()
     if(len(lst)%2 != 0):
         return lst[len(lst)//2]
@@ -22,48 +22,9 @@ def mymedian(lst):
         return (lst[(len(lst)//2)-1] + lst[len(lst)//2])/2
 
 
-def mymode(lst):
+def mode(lst):
     return max(set(lst), key=lst.count)
 
 
-if __name__ == "__main__":
-    import numpy as np
-    speed = [32,111,138,28,59,77,97]
-
-    # checking mystd
-    std = np.std(speed)
-    mystd = mystd(speed)
-    if mystd == std:
-        print('mystd is ok...')
-    else:
-        print('something went wrong in mystd...')
-    del std, mystd
-
-    # checking myvar
-    var = np.var(speed)
-    myvar = myvar(speed)
-    if myvar == var:
-        print('myvar is ok...')
-    else:
-        print('something went wrong in myvar...')
-    del var, myvar
-
-    # checking mymean
-    mean = np.mean(speed)
-    mymean = mymean(speed)
-    if mymean == mean:
-        print('mymean is ok...')
-    else:
-        print('something went wrong in mymean...')
-    del mean, mymean
-    
-    # checking mymedian
-    median = np.median(speed)
-    mymedian = mymedian(speed)
-    if mymedian == median:
-        print('mymedian if ok...')
-    else:
-        print('something went wrong in mymedian...')
-    del mymedian, median
-
-    
+def range(lst):
+    return max(lst)-min(lst)
